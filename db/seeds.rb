90ba51348855ca7d870875b8f8329890086d99c1
Brayan Diaz C <brayandiazc@gmail.com>
2023-07-13 13:07:12 -0500

    feat: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating categories...'
+
+5.times do |i|
+  Category.create(name: "Category #{i}")
+end
+
+puts 'Creating users...'
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456',
+    name: Faker::Name.name,
+    phone: Faker::PhoneNumber.phone_number,
+    age: Faker::Number.between(from: 18, to: 65)
+  )
+end
+
+puts 'Creating posts...'
+
+50.times do
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    category_id: rand(1..5),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    user_id: rand(1..10)
+  )
+end
